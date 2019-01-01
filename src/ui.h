7cfa0a48debcb1e1a73b4d71d3279da6d8662f9d
Akuli <akuviljanen17@gmail.com>
2019-01-01 22:34:51 +0200

    document UiSelection properly

--- src/ui.h
+++ src/ui.h
@@ -5,9 +5,31 @@
 #include "card.h"   // IWYU pragma: keep
 #include "sol.h"
 
-// represents the card pile or location that the user has selected
-// a pointer to struct Card is not enough because it's possible to select a
-// place where cards can be put, but that currently contains no cards
+/*
+represents the card pile or location that the user has selected
+a pointer to struct Card is not enough because it's possible to select a
+place where cards can be put, but that currently contains no cards
+
+possible values:
+
+	.card = NULL, .place = 0
+		nothing selected
+
+	.card = NULL, .place = SOL_STOCK
+		stock selected
+
+	.card = NULL, .place = SOL_DISCARD
+		discard selected
+
+	.card = NULL, .place = SOL_FOUNDATION(n)
+		n'th foundation selected
+
+	.card != NULL, .place = SOL_TABLEAU(n)      where 0 <= n < 4
+		tableau n selected, bottommost selected card is the card
+
+	.card = NULL, .place = SOL_TABLEAU(n)       where 0 <= n < 4
+		tableau n selected, there are no cards in that tableau
+*/
 struct UiSelection {
 	struct Card *card;   // NULL for nothing selected
 	SolCardPlace place;  // 0 for nothing selected
