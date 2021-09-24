0413ed5f230ffd4e4434439bb578c1101316f93f
Akuli <akuviljanen17@gmail.com>
2021-09-24 14:47:07 +0300

    ui.cpp cleanup

--- src/ui.cpp
+++ src/ui.cpp
@@ -24,128 +24,133 @@ void ui_initcolors()
 }
 
 // ui_x() and ui_y() convert coordinates from card counts to curses coordinates
-static inline int ui_x(int xcnt, int w)
+static inline int ui_x(int x_count, int terminal_width)
 {
 	// evenly spaced 7 columns, centered
 	//
-	// space per column = w/7
-	// center of column = space per column * (xcnt + 1/2)
+	// space per column = terminal_width/7
+	// center of column = space per column * (x_count + 1/2)
 	// result = center of column - CARD_WIDTH/2
 	//
 	// simplifying it gives this, i wrote it with just 1 division to get only 1
 	// floordiv and hopefully less little errors, could have also used floats but
 	// this works fine
-	return (2*xcnt*w + w - 7*CARD_WIDTH)/(2*7);
+	return ((2*x_count + 1)*terminal_width - 7*CARD_WIDTH)/(2*7);
 }
 
-static inline int ui_y(int ycnt, int h)
+static inline int ui_y(int y_count)
 {
 	// start at first row, then no blank rows in between
 	// because the line drawing characters look blanky enough anyway
-	return ycnt*CARD_HEIGHT;
+	return y_count*CARD_HEIGHT;
 }
-
-// because i need 2 kind of borders, "normal" and "selected"
-struct Border {
-	// e.g. hrz = horizontal, ul = upper left
-	const std::string hrz, vrt, ul, ur, ll, lr;
-};
 
 // https://en.wikipedia.org/wiki/Box-drawing_character#Unicode
-static const Border normal_border = { "─", "│", "╭", "╮", "╰", "╯" };
-static const Border selected_border = { "═", "║", "╔", "╗", "╚", "╝" };
+struct Border {
+	const char *horizontal, *vertical, *upper_left, *upper_right, *lower_left, *lower_right;
+};
+static constexpr Border normal_border = { "─", "│", "╭", "╮", "╰", "╯" };
+static constexpr Border selected_border = { "═", "║", "╔", "╗", "╚", "╝" };
 
-// box() is annoyingly for subwindows only
-static void draw_box(WINDOW *win, int xstart, int ystart, int w, int h, char bg, bool sel)
-{
-	Border b = sel ? selected_border : normal_border;
+// curses box() is annoyingly for subwindows only
+struct Box {
+	int left, top, width, height;
+	bool selected;
 
-	mvwaddstr(win, ystart, xstart, b.ul.c_str());
-	for (int x=xstart+1; x < xstart+w-1; x++)
-		mvwaddstr(win, ystart, x, b.hrz.c_str());
-	mvwaddstr(win, ystart, xstart+w-1, b.ur.c_str());
+	int right() const { return left + width; }
+	int bottom() const { return top + height; }
 
-	for (int y=ystart+1; y < ystart+h-1; y++) {
-		mvwaddstr(win, y, xstart, b.vrt.c_str());
-		for (int x=xstart+1; x < xstart+w-1; x++)
-			mvwaddch(win, y, x, bg);
-		mvwaddstr(win, y, xstart+w-1, b.vrt.c_str());
+	void draw(WINDOW *window, char background) const {
+		const Border& border = this->selected ? selected_border : normal_border;
+
+		mvwaddstr(window, this->top, this->left, border.upper_left);
+		for (int x = this->left + 1; x < this->right() - 1; x++)
+			mvwaddstr(window, this->top, x, border.horizontal);
+		mvwaddstr(window, this->top, this->right()-1, border.upper_right);
+
+		for (int y=this->top+1; y < this->bottom() - 1; y++) {
+			mvwaddstr(window, y, this->left, border.vertical);
+			for (int x = this->left + 1; x < this->right() - 1; x++)
+				mvwaddch(window, y, x, background);
+			mvwaddstr(window, y, this->right() - 1, border.vertical);
+		}
+
+		mvwaddstr(window, this->bottom() - 1, this->left, border.lower_left);
+		for (int x = this->left + 1; x < this->right() - 1; x++)
+			mvwaddstr(window, this->bottom() - 1, x, border.horizontal);
+		mvwaddstr(window, this->bottom() - 1, this->right() - 1, border.lower_right);
 	}
+};
 
-	mvwaddstr(win, ystart+h-1, xstart, b.ll.c_str());
-	for (int x=xstart+1; x < xstart+w-1; x++)
-		mvwaddstr(win, ystart+h-1, x, b.hrz.c_str());
-	mvwaddstr(win, ystart+h-1, xstart+w-1, b.lr.c_str());
-}
-
-// draws card on win
-// xo and yo offsets as curses units, for drawing overlapping cards
-//
 // newwin() doesn't work because partially erasing borders is surprisingly tricky
 // partial erasing is needed for cards that are on top of cards
 // since we can't use subwindow borders, they're not very helpful
-static void draw_card(WINDOW *win, const Card *card, int xstart, int ystart, bool sel, bool color)
+static void draw_card(WINDOW *window, const Card *card, int left, int top, bool selected, bool color)
 {
-	if (card || sel)
-		draw_box(win, xstart, ystart, CARD_WIDTH, CARD_HEIGHT, (!card || card->visible) ? ' ' : '?', sel);
-	if (!card)
-		return;
+	Box box = { left, top, CARD_WIDTH, CARD_HEIGHT, selected };
+	if (card || selected) {
+		char background = (card && !card->visible) ? '?' : ' ';
+		box.draw(window, background);
+	}
 
-	if (card->visible) {
-		// underlying values of SuitColor are valid color pair numbers
-		int attr = COLOR_PAIR(card->suit.color().color_pair_number());
+	if (card && card->visible) {
+		int attributes = COLOR_PAIR(card->suit.color().color_pair_number());
 		if (color)
-			wattron(win, attr);
-		mvaddstr(ystart+1, xstart+1, card->number_string().c_str());
-		mvaddstr(ystart+1, xstart+CARD_WIDTH-2, card->suit.string().c_str());
-		mvaddstr(ystart+CARD_HEIGHT-2, xstart+1, card->suit.string().c_str());
-		mvaddstr(ystart+CARD_HEIGHT-2, xstart+CARD_WIDTH-1-card->number_string().length(), card->number_string().c_str());
+			wattron(window, attributes);
+
+		std::string number_string = card->number_string();
+		std::string suit_string = card->suit.string();
+		mvaddstr(top+1, left+1, number_string.c_str());
+		mvaddstr(box.bottom()-2, box.right() - 1 - number_string.length(), number_string.c_str());
+		mvaddstr(top+1, box.right()-2, suit_string.c_str());
+		mvaddstr(box.bottom()-2, left+1, suit_string.c_str());
+
 		if (color)
-			wattroff(win, attr);
+			wattroff(window, attributes);
 	}
 }
 
 // unlike a simple for loop, handles overflow
-static void draw_card_stack(WINDOW *win, const Card *botcrd, int xstart, int ystart, const Card *firstsel, bool color)
+static void draw_card_stack(WINDOW *window, const Card *bottom_card, int left, int top, const Card *first_selected, bool color)
 {
-	if (!botcrd)
+	if (!bottom_card)
 		return;
 
-	int w, h;
-	getmaxyx(win, h, w);
-	(void)w;   // suppress warning about unused var
+	int terminal_width, terminal_height;
+	getmaxyx(window, terminal_height, terminal_width);
+	(void)terminal_width;   // suppress warning about unused var
 
-	// the text (number and suit) of botcrd is at ystart+1
+	// the text (number and suit) of bottom_card is at top+1
 	// let's figure out where it is for the topmost card
-	int toptxty = ystart+1;
-	int ncardstotal = 1;
-	for (Card *card = botcrd->next /* botcrd is already counted */ ; card; card = card->next) {
-		toptxty += Y_OFFSET_BIG;
-		ncardstotal++;
+	int top_text_y = top+1;
+	int total_card_count = 1;
+	for (Card *card = bottom_card->next /* bottom_card already counted */ ; card; card = card->next) {
+		top_text_y += Y_OFFSET_BIG;
+		total_card_count++;
 	}
 
 	// we can make all cards visible by displaying some cards with a smaller offset
-	// we'll display n cards with the bigger offset
-	int n = ncardstotal;
-	while (toptxty >= h) {
-		toptxty -= Y_OFFSET_BIG;
-		toptxty += Y_OFFSET_SMALL;
-		n--;
+	// we'lower_left display visible_card_count cards with the bigger offset
+	int visible_card_count = total_card_count;
+	while (top_text_y >= terminal_height) {
+		top_text_y -= Y_OFFSET_BIG;
+		top_text_y += Y_OFFSET_SMALL;
+		visible_card_count--;
 	}
 
 	// to give some extra room that wouldn't be really necessary, but is nicer
 	// without the if, cards get stacked even when there's enough room
-	if (n != ncardstotal)
-		n--;
+	if (visible_card_count != total_card_count)
+		visible_card_count--;
 
 	// let's draw the cards
-	bool sel = false;
-	int y = ystart;
-	for (const Card *card = botcrd; card; card = card->next) {
-		if (card == firstsel)
-			sel = true;
-		draw_card(win, card, xstart, y, sel, color);
-		y += (--n > 0) ? Y_OFFSET_BIG : Y_OFFSET_SMALL;
+	bool selected = false;
+	int y = top;
+	for (const Card *card = bottom_card; card; card = card->next) {
+		if (card == first_selected)
+			selected = true;
+		draw_card(window, card, left, y, selected, color);
+		y += (--visible_card_count > 0) ? Y_OFFSET_BIG : Y_OFFSET_SMALL;
 	}
 }
 
@@ -153,69 +158,69 @@ static void draw_card_stack(WINDOW *win, const Card *botcrd, int xstart, int yst
 enum class DiscardHide { HIDE_ALL, SHOW_LAST_ONLY, SHOW_ALL };
 
 // TODO: this function is quite long, break it up
-static void draw_the_klon(WINDOW *win, const Klondike& klon, const Selection& sel, bool moving, bool color, DiscardHide dh, int dscxoff)
+static void draw_game(WINDOW *window, const Klondike& klon, const Selection& sel, bool moving, bool color, DiscardHide discard_hide, int discard_x_offset)
 {
-	werase(win);
+	werase(window);
 
-	int w, h;
-	getmaxyx(win, h, w);
+	int terminal_width, terminal_height;
+	getmaxyx(window, terminal_height, terminal_width);
 
 	// drawing just one card is enough for this
-	draw_card(win, klon.stock, ui_x(0, w), ui_y(0, h), sel.place == CardPlace::stock(), color);
+	draw_card(window, klon.stock, ui_x(0, terminal_width), ui_y(0), sel.place == CardPlace::stock(), color);
 
-	int nshowdis = klon.discardshow;
-	if (sel.place == CardPlace::discard() && moving)  // user is moving a detached card, and it's in the discard
-		nshowdis++;
+	int cards_shown_in_discard = klon.discardshow;
+	if (sel.place == CardPlace::discard() && moving)
+		cards_shown_in_discard++;
 
-	int x = ui_x(1, w);
-	for (Card *card = cardlist::top_n(klon.discard, nshowdis); card; (card = card->next), (x += dscxoff)) {
-		Card crdval = *card;
+	int x = ui_x(1, terminal_width);
+	for (Card *card = cardlist::top_n(klon.discard, cards_shown_in_discard); card; (card = card->next), (x += discard_x_offset)) {
+		Card temp_card = *card;
 
-		assert(crdval.visible);
-		if (dh == DiscardHide::HIDE_ALL)
-			crdval.visible = false;
-		if (dh == DiscardHide::SHOW_LAST_ONLY)
-			crdval.visible = !card->next;
+		assert(temp_card.visible);
+		if (discard_hide == DiscardHide::HIDE_ALL)
+			temp_card.visible = false;
+		if (discard_hide == DiscardHide::SHOW_LAST_ONLY)
+			temp_card.visible = !card->next;
 
-		draw_card(win, &crdval, x, ui_y(0, h), sel.place == CardPlace::discard() && !card->next, color);
+		draw_card(window, &temp_card, x, ui_y(0), sel.place == CardPlace::discard() && !card->next, color);
 	}
 
 	if (!klon.discard)   // nothing was drawn, but if the discard is selected, at least draw that
-		draw_card(win, nullptr, ui_x(1, w), ui_y(0, h), sel.place == CardPlace::discard(), color);
+		draw_card(window, nullptr, ui_x(1, terminal_width), ui_y(0), sel.place == CardPlace::discard(), color);
 
 	// foundations are similar to discard
 	for (int i=0; i < 4; i++)
-		draw_card(win, cardlist::top(klon.foundations[i]), ui_x(3+i, w), ui_y(0, h), sel.place == CardPlace::foundation(i), color);
+		draw_card(window, cardlist::top(klon.foundations[i]), ui_x(3+i, terminal_width), ui_y(0), sel.place == CardPlace::foundation(i), color);
 
 	// now the tableau... here we go
 	for (int x=0; x < 7; x++) {
 		if (!klon.tableau[x]) {
 			// draw a border if the tableau item is selected
-			draw_card(win, nullptr, ui_x(x, w), ui_y(1, h), sel.place == CardPlace::tableau(x), color);
+			draw_card(window, nullptr, ui_x(x, terminal_width), ui_y(1), sel.place == CardPlace::tableau(x), color);
 			continue;
 		}
 
-		int yo = 0;
+		int y = ui_y(1);
 		for (Card *card = klon.tableau[x]; card; card = card->next) {
 			if (card->visible) {
-				draw_card_stack(win, card, ui_x(x, w), ui_y(1, h) + yo, sel.card, color);
+				draw_card_stack(window, card, ui_x(x, terminal_width), y, sel.card, color);
 				break;
 			}
 
-			draw_card(win, card, ui_x(x, w), ui_y(1, h) + yo, false, color);
-			yo += Y_OFFSET_SMALL;
+			draw_card(window, card, ui_x(x, terminal_width), y, false, color);
+			y += Y_OFFSET_SMALL;
 		}
 	}
 
 	if (klon.win()) {
 		std::string msg = "you win :)";
-		mvwaddstr(win, h/2, (w - msg.length())/2, msg.c_str());
+		mvwaddstr(window, terminal_height/2, (terminal_width - msg.length())/2, msg.c_str());
 	}
 }
 
-static DiscardHide decide_what_to_hide(const SelectionOrMove& selmv, bool cmdlnopt)
+static DiscardHide decide_what_to_hide(const SelectionOrMove& selmv, bool command_line_option)
 {
-	if (!cmdlnopt)
+	if (!command_line_option)
 		return DiscardHide::SHOW_ALL;
 
 	if (!selmv.ismove)
@@ -228,17 +233,17 @@ static DiscardHide decide_what_to_hide(const SelectionOrMove& selmv, bool cmdlno
 	return DiscardHide::SHOW_LAST_ONLY;
 }
 
-void ui_drawklon(WINDOW *win, const Klondike& klon, const SelectionOrMove& selmv, bool color, bool discardhide)
+void ui_drawklon(WINDOW *window, const Klondike& klon, const SelectionOrMove& selmv, bool color, bool discardhide)
 {
-	DiscardHide dh = decide_what_to_hide(selmv, discardhide);
-	int dscxoff = discardhide ? 1 : X_OFFSET;
+	DiscardHide discard_hide = decide_what_to_hide(selmv, discardhide);
+	int discard_x_offset = discardhide ? 1 : X_OFFSET;
 
 	if (selmv.ismove) {
 		std::array<Card, 13*4> temp_cards;
 		Klondike temp_klon;
 		Selection temp_sel = { klon.dup(temp_klon, selmv.move.card, temp_cards), selmv.move.dest };
 		temp_klon.move(temp_sel.card, temp_sel.place, true);
-		draw_the_klon(win, temp_klon, temp_sel, true, color, dh, dscxoff);
+		draw_game(window, temp_klon, temp_sel, true, color, discard_hide, discard_x_offset);
 	} else
-		draw_the_klon(win, klon, selmv.sel, false, color, dh, dscxoff);
+		draw_game(window, klon, selmv.sel, false, color, discard_hide, discard_x_offset);
 }
