12d53df85fe8dc80c50488f3952c2d0385edfc16
Akuli <akuviljanen17@gmail.com>
2025-06-14 23:45:27 +0300

    Port scroll

--- src/scroll.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SCROLL_H
-#define SCROLL_H
-
-#include <curses.h>
-
-// see newpad() man page for more info about pads vs "normal" windows
-void show_pad_with_scrolling(WINDOW *window, WINDOW *pad);
-
-#endif
